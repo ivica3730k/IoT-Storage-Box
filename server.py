5ec6b1111eac4bd92c8282181aa67f184fc9fa54
IcE <ivica-matic@outlook.com>
2020-10-23 00:34:50 +0100

    Implemented ability to put data

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-import database as db
 import settings
+from flask import request
+import json
+import database as db
 
 app = settings.app
 
@@ -7,6 +9,12 @@ app = settings.app
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/<apikey>/put/')
+def put(apikey: str):
+    db.write_data(apikey, json.dumps(dict(request.args)))
+    return apikey
 
 
 app.run(debug=True, use_reloader=True)
