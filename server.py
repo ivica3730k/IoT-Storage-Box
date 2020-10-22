4f8a904ccfaf34d875ae11df728d123b23ae3f9f
IcE <ivica-matic@outlook.com>
2020-10-22 23:49:50 +0100

    Created minimal database interface functions

--- server.py
+++ server.py
@@ -21,3 +21,72 @@ class Data(db.Model):
     key = db.Column(db.String(36), db.ForeignKey('apikey.id'))
     timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     data = db.Column(db.Text())
+
+
+def create_api_key(api_key: str) -> ApiKey:
+    """
+
+    :param api_key:
+    :return:
+    """
+    key = ApiKey()
+    key.id = api_key
+    db.session.add(key)
+    db.session.commit()
+    return key
+
+
+def api_key_exist(api_key: str) -> bool:
+    """
+
+    :param api_key:
+    :return:
+    """
+    try:
+        val = ApiKey.query.filter_by(id=api_key)[0]
+        return True
+    except IndexError:
+        return False
+
+
+def get_api_key(api_key: str) -> ApiKey:
+    """
+
+    :param api_key:
+    :return:
+    """
+    try:
+        return ApiKey.query.filter_by(id=api_key)[0]
+    except IndexError as e:
+        raise ValueError("API Key not present in database") from e
+
+
+def remove_api_key(api_key: str) -> None:
+    """
+
+    :param api_key:
+    :return:
+    """
+    key = get_api_key(api_key)
+    db.session.delete(key)
+    db.session.commit()
+
+
+def write_data(api_key: str, data) -> None:
+    """
+
+    :param api_key:
+    :param data:
+    :return:
+    """
+    if api_key_exist(api_key):
+        key = get_api_key(api_key)
+    else:
+        key = create_api_key(api_key)
+    newDataObject = Data()
+    newDataObject.key = key
+    newDataObject.data = data
+    key.data.append(newDataObject)
+    db.session.add(key)
+    db.session.add(newDataObject)
+    db.session.commit()
